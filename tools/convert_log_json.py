69adc8aa165eea3419bf9af8a79982b4c73545a5
Vladislav_Laptev <vvl39@tpu.ru>
2022-02-10 22:06:02 +0700

    Revert "Update convert_log_json.py"
    This reverts commit cc49e628063d17467bcc180875dd8babd6b87dd0.

--- tools/convert_log_json.py
+++ tools/convert_log_json.py
@@ -1,44 +1,41 @@
 import os
 import argparse
+
 import pandas as pd
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Coverter logs json to csv')
-    parser.add_argument('--json-path', default='../work_dirs/fcn_m-v2-d8_512x1024_80k_cityscapes/None.log.json')
-    # parser.add_argument('--json-path', default='../work_dirs/20220210_011806.log.json')
-    parser.add_argument('--output-dir', default='../work_dirs/fcn_m-v2-d8_512x1024_80k_cityscapes')
-    parser.add_argument('--colums-train', default=['memory',
-                                                   'data_time',
-                                                   'decode.loss_ce',
-                                                   'decode.acc_seg',
-                                                   'aux.loss_ce',
-                                                   'aux.acc_seg',
-                                                   'loss'
-                                                   ])
-    parser.add_argument('--colums-obligatory', default=['mode',
-                                                        'epoch',
-                                                        'iter',
-                                                        'lr'])
-    parser.add_argument('--colums-delete', default=['seed',
-                                                    'env_info',
-                                                    'exp_name',
-                                                    'mmseg_version',
-                                                    'config',
-                                                    'CLASSES',
-                                                    'PALETTE',
-                                                    'loss',
-                                                    'time'])
+
+    COLUMNS = [
+        'mode',
+        'epoch',
+        'iter',
+        'lr',
+        'memory',
+        'data_time',
+        'time',
+        'decode.loss_ce',
+        'decode.acc_seg',
+        'aux.loss_ce',
+        'aux.acc_seg',
+        'loss',
+    ]
+
+    parser = argparse.ArgumentParser(description='Convert JSON logs to CSV')
+    parser.add_argument('--json_path', required=True, type=str)
+    parser.add_argument('--save_dir', required=True, type=str)
+    parser.add_argument('--columns_train', default=COLUMNS)
     args = parser.parse_args()
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
     dataframe = pd.read_json(args.json_path, lines=True)
     for mode in dataframe['mode'].unique():
         if mode == mode:
             df = dataframe.loc[dataframe['mode'] == mode]
             if mode != 'train':
-                df = df.drop(columns=[col for col in list(dataframe.columns) if col in args.colums_train +
-                                      args.colums_delete], axis=1)
+                df = df.drop(columns=args.columns_train+['mode'], axis=1)
             else:
-                df = df.drop(columns=[col for col in list(dataframe.columns) if col not in args.colums_train +
-                                      args.colums_obligatory], axis=1)
-            df.to_csv(f'{args.output_dir}/{mode}.csv')
+                df = df.drop(columns=[col for col in list(dataframe.columns) if col not in args.columns_train], axis=1)
+            df.to_csv(f'{args.save_dir}/{mode}.csv')
