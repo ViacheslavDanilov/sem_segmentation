2c56eee2a24de55e901b73864372d9dd54ee056b
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-02-10 23:37:48 +0700

    [Fix]: PEP8 compliant refactoring and update of the column list

--- tools/convert_log_json.py
+++ tools/convert_log_json.py
@@ -1,47 +1,96 @@
 import os
+import logging
 import argparse
+from typing import List
+from pathlib import Path
 
 import pandas as pd
 
+logger = logging.getLogger(__name__)
+
+
+def main(
+        json_path: str,
+        columns_common: List[str],
+        columns_train: List[str],
+        columns_delete: List[str],
+        save_dir: str,
+) -> None:
+
+    df_src = pd.read_json(json_path, lines=True)
+    for mode in ['train', 'val']:
+        df = df_src[df_src['mode'] == mode]
+        if mode != 'train':
+            df = df.drop(
+                columns=[
+                    col for col in list(df_src.columns) if col in columns_train + columns_delete
+                ],
+                axis=1,
+            )
+        else:
+            df = df.drop(
+                columns=[
+                    col for col in list(df_src.columns) if col not in columns_train + columns_common
+                ],
+                axis=1,
+            )
+        os.makedirs(save_dir, exist_ok=True)
+        save_path = os.path.join(save_dir, '{:s}.xlsx'.format(mode))
+        df.to_excel(save_path, sheet_name='Meta', index=False, startrow=0, startcol=0)
+        logger.info('{:s} log saved: {:s}'.format(mode.capitalize(), save_path))
+
+
 if __name__ == "__main__":
-    COLUMNS_TRAIN = ['memory',
-                    'data_time',
-                    'decode.loss_ce',
-                    'decode.acc_seg',
-                    'aux.loss_ce',
-                    'aux.acc_seg',
-                    'loss']
-    COLUMNS_OBLIGATORY = ['mode',
-                         'epoch',
-                         'iter',
-                         'lr']
-    COLUMNS_DELETE = ['seed',
-                     'env_info',
-                     'exp_name',
-                     'mmseg_version',
-                     'config',
-                     'CLASSES',
-                     'PALETTE',
-                     'time']
+
+    os.makedirs('logs', exist_ok=True)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%d.%m.%Y %I:%M:%S',
+        filename='logs/{:s}.log'.format(Path(__file__).stem),
+        filemode='w',
+        level=logging.INFO,
+    )
+
+    columns_common = [
+        'mode',
+        'epoch',
+        'iter',
+        'lr',
+    ]
+
+    columns_train = [
+        'memory',
+        'decode.loss_ce',
+        'decode.acc_seg',
+        'aux.loss_ce',
+        'aux.acc_seg',
+        'loss',
+    ]
+
+    columns_delete = [
+        'seed',
+        'env_info',
+        'exp_name',
+        'data_time',
+        'mmseg_version',
+        'config',
+        'CLASSES',
+        'PALETTE',
+        'time',
+    ]
 
     parser = argparse.ArgumentParser(description='Convert JSON logs to CSV')
     parser.add_argument('--json_path', required=True, type=str)
     parser.add_argument('--save_dir', required=True, type=str)
-    parser.add_argument('--columns_train', default=COLUMNS_TRAIN)
-    parser.add_argument('--columns_obligatory', default=COLUMNS_OBLIGATORY)
-    parser.add_argument('--columns_delete', default=COLUMNS_DELETE)
+    parser.add_argument('--columns_common', default=columns_common)
+    parser.add_argument('--columns_train', default=columns_train)
+    parser.add_argument('--columns_delete', default=columns_delete)
     args = parser.parse_args()
 
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
-
-    dataframe = pd.read_json(args.json_path, lines=True)
-    for mode in ['train', 'val']:
-        df = dataframe[dataframe['mode'] == mode]
-        if mode != 'train':
-            df = df.drop(columns=[col for col in list(dataframe.columns) if col in args.columns_train +
-                                  args.columns_delete], axis=1)
-        else:
-            df = df.drop(columns=[col for col in list(dataframe.columns) if col not in args.columns_train +
-                                  args.columns_obligatory], axis=1)
-        df.to_csv(f'{args.save_dir}/{mode}.csv')
+    main(
+        json_path=args.json_path,
+        columns_common=args.columns_common,
+        columns_train=args.columns_train,
+        columns_delete=args.columns_delete,
+        save_dir=args.save_dir,
+    )
