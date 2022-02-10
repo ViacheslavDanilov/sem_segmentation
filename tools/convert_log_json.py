58680a2f60d1d92a948d17b5b6ba852be542018b
Vladislav_Laptev <vvl39@tpu.ru>
2022-02-10 22:38:17 +0700

    Update convert_log_json.py

--- tools/convert_log_json.py
+++ tools/convert_log_json.py
@@ -4,18 +4,18 @@ import argparse
 import pandas as pd
 
 if __name__ == "__main__":
-    COLUMS_TRAIN = ['memory',
+    COLUMNS_TRAIN = ['memory',
                     'data_time',
                     'decode.loss_ce',
                     'decode.acc_seg',
                     'aux.loss_ce',
                     'aux.acc_seg',
                     'loss']
-    COLUMS_OBLIGATORY = ['mode',
+    COLUMNS_OBLIGATORY = ['mode',
                          'epoch',
                          'iter',
                          'lr']
-    COLUMS_DELETE = ['seed',
+    COLUMNS_DELETE = ['seed',
                      'env_info',
                      'exp_name',
                      'mmseg_version',
@@ -27,9 +27,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert JSON logs to CSV')
     parser.add_argument('--json_path', required=True, type=str)
     parser.add_argument('--save_dir', required=True, type=str)
-    parser.add_argument('--colums_train', default=COLUMS_TRAIN)
-    parser.add_argument('--colums_obligatory', default=COLUMS_OBLIGATORY)
-    parser.add_argument('--colums_delete', default=COLUMS_DELETE)
+    parser.add_argument('--columns_train', default=COLUMNS_TRAIN)
+    parser.add_argument('--columns_obligatory', default=COLUMNS_OBLIGATORY)
+    parser.add_argument('--columns_delete', default=COLUMNS_DELETE)
     args = parser.parse_args()
 
     if not os.path.exists(args.save_dir):
@@ -37,11 +37,11 @@ if __name__ == "__main__":
 
     dataframe = pd.read_json(args.json_path, lines=True)
     for mode in ['train', 'val']:
-        df = dataframe.loc[dataframe['mode'] == mode]
+        df = dataframe[dataframe['mode'] == mode]
         if mode != 'train':
-            df = df.drop(columns=[col for col in list(dataframe.columns) if col in args.colums_train +
-                                  args.colums_delete], axis=1)
+            df = df.drop(columns=[col for col in list(dataframe.columns) if col in args.columns_train +
+                                  args.columns_delete], axis=1)
         else:
-            df = df.drop(columns=[col for col in list(dataframe.columns) if col not in args.colums_train +
-                                  args.colums_obligatory], axis=1)
+            df = df.drop(columns=[col for col in list(dataframe.columns) if col not in args.columns_train +
+                                  args.columns_obligatory], axis=1)
         df.to_csv(f'{args.save_dir}/{mode}.csv')
