b9b89aac28093b7455b034b1d0bae9d7f0353260
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-02-17 00:02:40 +0700

    [Refactoring]: minor fixes

--- tools/update_mask.py
+++ tools/update_mask.py
@@ -1,25 +1,19 @@
 import json
 import shutil
 import argparse
-import datetime
 
 from tqdm import tqdm
 
+from utils import get_current_time
 from tools.supervisely_utils import *
 
 logger = logging.getLogger(__name__)
 
 
-def utc_now():
-    utc_time = datetime.datetime.utcnow()
-    utc_time = utc_time.replace(tzinfo=datetime.timezone.utc)
-    return utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-
 def main(
         input_dir: str,
-        classes_border: dict,
-        classes_filling: dict,
+        class_groups: dict,
+        class_ids: dict,
         df_project: pd.DataFrame,
         output_dir: str,
 ) -> None:
@@ -32,44 +26,46 @@ def main(
         shutil.copyfile(f'{input_dir}/meta.json', f'{output_dir}/meta.json')
         shutil.copyfile(f'{input_dir}/obj_class_to_machine_color.json', f'{output_dir}/obj_class_to_machine_color.json')
 
-        for idx, row in tqdm(df_dataset.iterrows(), desc='Mask update', unit=' ann'):
+        for idx, row in tqdm(df_dataset.iterrows(), desc='Updating Supervisely dataset', unit=' ann'):
             filename = row['filename']
             img_path = row['img_path']
+            img_name = Path(img_path).name
+            shutil.copyfile(f'{img_path}', f'{output_dir}/{dataset}/img/{img_name}')
+
             ann_path = row['ann_path']
-            shutil.copyfile(f'{img_path}', f'{output_dir}/{dataset}/img/{os.path.basename(img_path)}')
+            ann_name = Path(ann_path).name
             f = open(ann_path)
             ann_data = json.load(f)
 
             if len(ann_data['objects']) > 0:
                 new_objects = []
                 for obj in ann_data['objects']:
-                    if obj['classTitle'] in classes_border:
-                        obj_mask64 = obj['bitmap']['data']
-                        obj_mask = base64_to_mask(obj_mask64)
-                        binary_mask = binary_fill_holes(obj_mask.copy(), structure=None)
-                        binary_mask = binary_opening(binary_mask, structure=None)
-                        binary_mask = 255 * binary_mask.astype(np.uint8)
-                        new_object_mask = binary_mask - obj_mask
+                    if obj['classTitle'] in class_groups:
+                        wall_mask64 = obj['bitmap']['data']
+                        wall_mask = base64_to_mask(wall_mask64)
+                        filled_mask = binary_fill_holes(wall_mask.copy(), structure=None)
+                        filled_mask = 255 * filled_mask.astype(np.uint8)
+                        lumen_mask = filled_mask - wall_mask
+
                         try:
-                            y = np.where(new_object_mask == 255)[0][0]
-                            x = np.where(new_object_mask == 255)[1][0]
-                            new_object_mask = mask_to_base64(new_object_mask)
-                            # # palette = get_palette(class_names)
+                            # Append a wall object
                             new_objects.append(obj)
-                            title = classes_border[obj['classTitle']]
+
+                            # Append a lumen object
+                            lumen_mask = mask_to_base64(lumen_mask)
+                            class_name = class_groups[obj['classTitle']]
                             new_objects.append(
                                 {
-                                    # 'id': 1,
-                                    'classId': classes_filling[title],
+                                    'classId': class_ids[class_name],
                                     'description': '',
                                     'geometryType': 'bitmap',
                                     'lablerLogin': 'Hunter_911',
-                                    'createdAt': utc_now(),
-                                    'updatedAt': utc_now(),
+                                    'createdAt': get_current_time(),
+                                    'updatedAt': get_current_time(),
                                     'tags': [],
-                                    'classTitle': title,
+                                    'classTitle': class_name,
                                     'bitmap': {
-                                        'data': new_object_mask,
+                                        'data': lumen_mask,
                                         'origin': [
                                             int(obj['bitmap']['origin'][0]),
                                             int(obj['bitmap']['origin'][1])
@@ -77,29 +73,41 @@ def main(
                                     }
                                 }
                             )
-                        except:
-                            logger.warning(f'The object {obj["classTitle"]} has no cavity, filename {filename}')
-                            continue
-                    elif obj['classTitle'] in classes_filling:
+
+                        except Exception as e:
+                            logger.info(f'The object {obj["classTitle"]} has no cavity. Filename: {filename}')
+
+                    elif obj['classTitle'] in class_ids:
                         continue
+
                     else:
                         new_objects.append(obj)
+
                 ann_data['objects'] = new_objects
-                with open(f'{output_dir}/{dataset}/ann/{os.path.basename(ann_path)}', 'w') as outfile:
+                with open(f'{output_dir}/{dataset}/ann/{ann_name}', 'w') as outfile:
                     json.dump(ann_data, outfile)
             else:
-                shutil.copyfile(f'{ann_path}', f'{output_dir}/{dataset}/ann/{os.path.basename(ann_path)}')
+                shutil.copyfile(f'{ann_path}', f'{output_dir}/{dataset}/ann/{ann_name}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    GROUP_CLASSES_1 = {
+    os.makedirs('logs', exist_ok=True)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%d.%m.%Y %I:%M:%S',
+        filename='logs/{:s}.log'.format(Path(__file__).stem),
+        filemode='w',
+        level=logging.INFO,
+    )
+
+    CLASS_GROUPS = {
         'Capillary wall': 'Capillary lumen',
         'Arteriole wall': 'Arteriole lumen',
         'Venule wall': 'Venule lumen',
     }
 
-    GROUP_CLASSES_2 = {
+    CLASS_IDS = {
         'Capillary lumen': 9945624,
         'Arteriole lumen': 9944173,
         'Venule lumen': 9944174,
@@ -120,8 +128,8 @@ if __name__ == "__main__":
 
     main(
         input_dir=args.project_dir,
-        classes_border=GROUP_CLASSES_1,
-        classes_filling=GROUP_CLASSES_2,
+        class_groups=CLASS_GROUPS,
+        class_ids=CLASS_IDS,
         df_project=df,
         output_dir=args.save_dir,
     )
