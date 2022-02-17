11aece3562b5d248cb13ef521f950741e9bc812b
Vladislav_Laptev <vvl39@tpu.ru>
2022-02-18 00:18:11 +0700

    Generation of lumen masks (#4)
    * Exclude logs dir
    
    * Update project name
    
    * Add utility functions
    
    * Add Supervisely utility functions
    
    * WIP: convert Supervisely dataset to Cityscapes
    
    * Create mapping function for class colors and IDs
    
    * WIP: convert Supervisely dataset to Cityscapes considering classes
    
    * Add functions used for mask processing and conversion
    
    * WIP: Update dataset conversion
    
    * Add option for saving indexed and colored masks
    
    * Add mask smoothing and filling using mathematical morphology
    
    * Create separate log dataset function
    
    * Update dataset split
    
    * [Fix]: change columns order
    
    * [Feature]: add OCR for modality info
    
    * [Feature]: add metadata extraction
    
    * [Feature]: include filename_template for file search
    
    * [Feature]: retrieve color palette by class names
    
    * [Feature]: revise the dataset conversion
    
    * Compute class weights
    
    * Add option to include/exclude empty masks
    
    * Save masks using the putpalette() method
    
    * [Fix]: add media files and update readme
    
    * [Refactoring]: variable renaming
    
    * [Fix]: change class colors, remove class_id, and fix mask smoothing
    
    * [Fix]: add origin coordinates to of an object and remove class_id
    
    * [Fix]: class_id is now calculated automatically based on args.class_names
    
    * Create update_mask.py
    
    * [Feature]: add get_current_time function
    
    * [Refactoring]: minor fixes
    
    * Update update_mask.py
    
    * [Fix]: fix processing of solid wall masks  and update the logger
    
    * [Fix]: update readme masks
    
    * [Fix]: rename the script
    
    Co-authored-by: Viacheslav Danilov <viacheslav.v.danilov@gmail.com>

---
+++ tools/generate_lumen_masks.py
@@ -0,0 +1,142 @@
+import json
+import shutil
+import argparse
+
+from tqdm import tqdm
+
+from utils import get_current_time
+from tools.supervisely_utils import *
+
+logger = logging.getLogger(__name__)
+
+
+def main(
+        input_dir: str,
+        class_groups: dict,
+        class_ids: dict,
+        df_project: pd.DataFrame,
+        output_dir: str,
+) -> None:
+
+    os.makedirs(output_dir) if not os.path.isdir(output_dir) else False
+    shutil.copyfile(f'{input_dir}/meta.json', f'{output_dir}/meta.json')
+    shutil.copyfile(f'{input_dir}/obj_class_to_machine_color.json', f'{output_dir}/obj_class_to_machine_color.json')
+
+    datasets = list(set(df_project.dataset))
+    for dataset in datasets:
+        df_dataset = df_project[df_project.dataset == dataset]
+        if not os.path.isdir(os.path.join(output_dir, dataset)):
+            os.makedirs(os.path.join(output_dir, dataset, 'img'))
+            os.makedirs(os.path.join(output_dir, dataset, 'ann'))
+
+        for idx, row in tqdm(df_dataset.iterrows(), desc='Updating Supervisely dataset', unit=' ann'):
+            filename = row['filename']
+            img_path = row['img_path']
+            img_name = Path(img_path).name
+            shutil.copyfile(f'{img_path}', f'{output_dir}/{dataset}/img/{img_name}')
+
+            ann_path = row['ann_path']
+            ann_name = Path(ann_path).name
+            f = open(ann_path)
+            ann_data = json.load(f)
+
+            if len(ann_data['objects']) > 0:
+                new_objects = []
+                for obj in ann_data['objects']:
+
+                    # Process walls
+                    if obj['classTitle'] in class_groups:
+                        wall_mask64 = obj['bitmap']['data']
+                        wall_mask = base64_to_mask(wall_mask64)
+                        filled_mask = binary_fill_holes(wall_mask.copy(), structure=None)
+                        filled_mask = 255 * filled_mask.astype(np.uint8)
+                        lumen_mask = filled_mask - wall_mask
+
+                        # Check the size of a lumen mask
+                        if cv2.countNonZero(lumen_mask) > 0:
+                            # Append a wall object
+                            new_objects.append(obj)
+
+                            # Append a lumen object
+                            lumen_mask = mask_to_base64(lumen_mask)
+                            class_name = class_groups[obj['classTitle']]
+                            new_objects.append(
+                                {
+                                    'classId': class_ids[class_name],
+                                    'description': '',
+                                    'geometryType': 'bitmap',
+                                    'lablerLogin': 'Hunter_911',
+                                    'createdAt': get_current_time(),
+                                    'updatedAt': get_current_time(),
+                                    'tags': [],
+                                    'classTitle': class_name,
+                                    'bitmap': {
+                                        'data': lumen_mask,
+                                        'origin': [
+                                            int(obj['bitmap']['origin'][0]),
+                                            int(obj['bitmap']['origin'][1])
+                                        ]
+                                    }
+                                }
+                            )
+                        else:
+                            logger.warning(f'The object {obj["classTitle"]} has no cavity, filename {filename}')
+
+                    # Process lumens
+                    elif obj['classTitle'] in class_ids:
+                        continue
+
+                    # Process other objects
+                    else:
+                        new_objects.append(obj)
+
+                ann_data['objects'] = new_objects
+                with open(f'{output_dir}/{dataset}/ann/{ann_name}', 'w') as outfile:
+                    json.dump(ann_data, outfile)
+            else:
+                shutil.copyfile(f'{ann_path}', f'{output_dir}/{dataset}/ann/{ann_name}')
+
+
+if __name__ == '__main__':
+
+    os.makedirs('logs', exist_ok=True)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%d.%m.%Y %I:%M:%S',
+        filename='logs/{:s}.log'.format(Path(__file__).stem),
+        filemode='w',
+        level=logging.INFO,
+    )
+
+    CLASS_GROUPS = {
+        'Capillary wall': 'Capillary lumen',
+        'Arteriole wall': 'Arteriole lumen',
+        'Venule wall': 'Venule lumen',
+    }
+
+    CLASS_IDS = {
+        'Capillary lumen': 9945624,
+        'Arteriole lumen': 9944173,
+        'Venule lumen': 9944174,
+    }
+
+    parser = argparse.ArgumentParser(description='Update mask dataset')
+    parser.add_argument('--project_dir', required=True, type=str)
+    parser.add_argument('--include_dirs', nargs='+', default=None, type=str)
+    parser.add_argument('--exclude_dirs', nargs='+', default=None, type=str)
+    parser.add_argument('--save_dir', required=True, type=str)
+    args = parser.parse_args()
+
+    df = read_sly_project(
+        project_dir=args.project_dir,
+        include_dirs=args.include_dirs,
+        exclude_dirs=args.exclude_dirs,
+    )
+
+    main(
+        input_dir=args.project_dir,
+        class_groups=CLASS_GROUPS,
+        class_ids=CLASS_IDS,
+        df_project=df,
+        output_dir=args.save_dir,
+    )
